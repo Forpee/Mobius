1f50ae528fab064f6f923c49fcf791b65ae84798
Forpee <khalilhassam3@gmail.com>
2022-05-27 19:15:48 +0200

    update

--- src/script.js
+++ src/script.js
@@ -112,7 +112,7 @@ const tick = () => {
     material.uniforms.uTime.value = elapsedTime;
 
     // Render
-    renderer.render(scene, camera);
+    // renderer.render(scene, camera);
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick);
